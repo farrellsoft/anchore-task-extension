a99226fa21d0d5dfb7059f6350af8b037aef18f9
xximjasonxx <imjason@gmail.com>
2019-11-02 20:41:21 -0600

    completed html write out code

--- anchoreScanTask/src/exporters/VulnScanHtmlExporter.ts
+++ anchoreScanTask/src/exporters/VulnScanHtmlExporter.ts
@@ -1,4 +1,5 @@
 import { VulnScanItem } from "../interfaces/VulnScanning";
+import fs from 'fs';
 
 export class VulnScanHtmlExporter {
     constructor(
@@ -6,6 +7,56 @@ export class VulnScanHtmlExporter {
     ) {}
 
     saveFile(filePath: string): void {
-        
+        const resultString = this.getHtmlStart() +
+            this.buildResultsTable() +
+            this.getHtmlEnd();
+
+        fs.writeFileSync(filePath, resultString);
+    }
+
+    private getHtmlStart(): string {
+        return `<!DOCTYPE html>
+            <html lang="en" >
+                <head>
+                    <title>Anchore Vulnerability Scan Results</title>
+                </head>
+
+                <body>
+                    <h2>Anchore Vulnerability Scan Results</h2>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Vulnerability ID</th>
+                                <th>Package</th>
+                                <th>Severity</th>
+                                <th>Fix</th>
+                            </tr>
+                        </thead>
+                        <tbody>`;
+                
+    }
+
+    private buildResultsTable(): string {
+        var resultTableString = "";
+        this.scanResults.forEach(scanResult => {
+            resultTableString += `
+                <tr>
+                    <td>
+                        <a href="${scanResult.url}">${scanResult.vuln}</a>
+                    </td>
+                    <td>${scanResult.package}</td>
+                    <td>${scanResult.severity.toString()}</td>
+                    <td>${scanResult.fix}</td>
+                </tr>`;
+        });
+
+        return resultTableString;
+    }
+
+    private getHtmlEnd(): string {
+        return `            </tbody>
+                        </table>
+                    </body>
+                </html>`;
     }
 }
